d4fd78ba9b41ef798da02caa8d4e6d78620fb611
scrumvisualize <scrumvisualize@gmail.com>
2022-05-25 23:49:10 +1000

    Added code on searchbox, news and loadrequests

--- src/components/admin/searchBox.js
+++ src/components/admin/searchBox.js
@@ -5,22 +5,50 @@ const data = [
     {
     "id" : 1,
     "name" : "Danile Victor",
-    "mobile": "3423224232"
+    "mobile": "3423224232",
+     processRequest: "Accept"
     },
     {
         "id" : 2,
         "name" : "Sam Philip",
-        "mobile": "2312312310"
+        "mobile": "2312312310",
+        processRequest: "Accept"
     },
     {
         "id" : 3,
         "name" : "Von Rodriguz",
-        "mobile": "5678999010"
+        "mobile": "5678999010",
+        processRequest: "Accept"
     },
     {
         "id" : 4,
         "name" : "Style Dorkin",
-        "mobile": "7896543212"
+        "mobile": "7896543212",
+        processRequest: "Accept"
+    },
+    {
+        "id" : 5,
+        "name" : "Rad",
+        "mobile": "9430001212",
+        processRequest: "Accept"
+    },
+    {
+        "id" : 6,
+        "name" : "Mat",
+        "mobile": "459943212",
+        processRequest: "Accept"
+    },
+    {
+        "id" : 7,
+        "name" : "Gow",
+        "mobile": "7559943202",
+        processRequest: "Accept"
+    },
+    {
+        "id" : 8,
+        "name" : "Tan",
+        "mobile": "5671234001",
+        processRequest: "Accept"
     }
 ]
 
@@ -29,6 +57,7 @@ const SearchBox = () => {
     const [requestList, setRequestList] = useState([]);
     const [searchResults, setSearchResults] = useState([]);
     const [processRequest, setProcessRequest] = useState("Accept");
+    const [loadRequests, setLoadRequests] = useState(3);
 
     const handleChange = (e) =>{
         setSearchTerm(e.target.value);
@@ -48,58 +77,107 @@ const SearchBox = () => {
         setSearchResults(results);
       }, [searchTerm, requestList]);
 
+      /* Loop through the requestList and check if item.id === e, then set to Save and return item */
+
       const processRequestData = (e) => {
-        const text = "Save";
-        setProcessRequest(text, e.target.id);
+        setRequestList(
+            requestList.map((item) => {
+                if (item.id === e) {
+                  item.processRequest = "Save";
+                }
+               return item;
+            })
+         );
       };
+
+      const declinePlayerRequest = (e) =>{
+        setRequestList(
+            requestList.map((item) => {
+                console.log(item.id);
+                if (item.id === e) {
+                    console.log("Which player::"+item.id);
+                  }
+                 return item;
+            })
+        );
+      }
+
+      const showMoreRequests = () => {
+        setLoadRequests((preValue) => preValue + 2);
+    }
 
     return (
         <div className="wrapper">
             <section className="searchMainBox">
                 <div className="row">
                     <label className='playerfield'>
-                        {/* <h3>Search Player Request</h3> */}
                         <div className="searchBox">
                             <input type="text" name="search" onChange={handleChange} placeholder='Search requests...'></input>
                         </div>
                     </label>
                 </div>
             </section>
+            <section class="col1">
+             <h3>Players Requests</h3>
+            {
+                        searchResults.slice(0, loadRequests).map(({id, name, mobile, processRequest }) => (
 
-            <section className="requestarea list">
-                {
-                        searchResults.map(({id, name, mobile}) => (
-                        <div className="playerRow"> 
-                            <label key={id}>
-                                <div className="row">
-                                    <div className="checkStyle1">
-                                        <input type="checkbox"></input>
+                        <div className='row'>
+                            <div className="playerRow"> 
+                                <label key={id}>
+                                    <div className="row">
+                                        <div className="checkStyle1">
+                                            <input type="checkbox"></input>
+                                        </div>
+                                        <div className="plyName">
+                                            <span>{name}</span>
+                                        </div>
+                                        <div className="plyMobile">
+                                            <span>{mobile}</span>
+                                        </div>
+                                        <div className="checkStyle2">
+                                            <input type="checkbox"></input>
+                                            <label>Member</label>
+                                        </div>
+                                        <div className="checkStyle3">
+                                            <input type="checkbox"></input>
+                                            <label>Active Player</label>
+                                        </div>
+                                        <div className="btnStyle4">
+                                            <button type="button" onClick={ (e) => processRequestData(id)}>{processRequest}</button>
+                                        </div>
+                                        <div className="btnStyle4">
+                                            <input type="button" onClick={(e) => declinePlayerRequest(id)} value="Decline Request"></input>
+                                        </div>
                                     </div>
-                                    <div className="plyName">
-                                        <span>{name}</span>
-                                    </div>
-                                    <div className="plyMobile">
-                                        <span>{mobile}</span>
-                                    </div>
-                                    <div className="checkStyle2">
-                                        <input type="checkbox"></input>
-                                        <label>Member</label>
-                                    </div>
-                                    <div className="checkStyle3">
-                                        <input type="checkbox"></input>
-                                        <label>Active Player</label>
-                                    </div>
-                                    <div className="btnStyle4">
-                                        <button type="button" onClick={ (e) => processRequestData(e)}>{processRequest}</button>
-                                    </div>
-                                    <div className="btnStyle4">
-                                        <input type="button" value="Decline Request"></input>
-                                    </div>
-                                </div>
-                            </label>
-                        </div>  
-                   ))}
+                                </label>
+                            </div>  
+                            
+                        </div>
+                       
+                   ))} 
+                   <div className="loadReqts">
+                        <button onClick={() => {
+                            showMoreRequests()
+                        }}>Load more...</button>
+                   </div>
             </section>
+            <section className="col2">
+            <h3>Announcement/ News</h3>
+                <div className='newsSection'>
+                    <div className='row'>
+                        <textarea>
+                        </textarea> 
+                    </div>
+                    <section className="col3">
+                    <button name="cancel">Cancel</button>  
+                    </section>   
+                    <section className="col4">
+                    <button name="submit">Submit</button> 
+                    </section> 
+                </div>
+            </section>
+            
         </div>  
     )
 }
